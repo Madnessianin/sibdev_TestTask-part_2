a50265a7b61e4973758b49bb70caec6fd114a44c
Madnessianin <madnessstalkerl@gmail.com>
2021-02-19 00:15:19 +0500

    fix bag css

--- src/Component/Page/Page.jsx
+++ src/Component/Page/Page.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Button, Col, Layout, Menu, Row } from 'antd';
 import './page.css'
-import { NavLink, Redirect, Route, Switch } from 'react-router-dom';
+import { NavLink, Redirect, Route, Switch, useLocation } from 'react-router-dom';
 import FavoritesConteiner from './Favorutes/FavoritesConteiner';
 import SearchConteiner from './Search/SearchConteiner';
 import logo from './../../assets/img/sibdev-logo.png'
@@ -9,7 +9,6 @@ const { Header, Content, Footer } = Layout;
 
 const Page = (props) => {
 
-    
     return (
         <Layout className="layout">
             <Header>
@@ -21,8 +20,8 @@ const Page = (props) => {
                     </Col>
                     <Col span={20}>
                         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-                                    <Menu.Item key="1"><NavLink to="/page/search">Поиск</NavLink></Menu.Item>
-                                    <Menu.Item key="2"><NavLink to="/page/favorites">Избранное</NavLink></Menu.Item>
+                            <Menu.Item key="1"><NavLink to="/page/search">Поиск</NavLink></Menu.Item>
+                            <Menu.Item key="2"><NavLink to="/page/favorites">Избранное</NavLink></Menu.Item>
                         </Menu>
                     </Col>
                     <Col span = {2}>
